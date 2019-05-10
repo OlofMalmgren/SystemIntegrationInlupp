bcc7f794357cfbf7cdec0e6e99d45239778b3353
ollebollebagare <45233794+ollebollebagare@users.noreply.github.com>
2019-05-10 09:57:57 +0200

    Create OlofConfig.h

---
+++ OlofConfig.h
@@ -0,0 +1,16 @@
+#define DEVICE_ID             WiFi.macAddress()     // already configured
+#define DEVICE_STUDENT        "Olof"
+#define DEVICE_TYPE           "NodeMCU1"
+#define DEVICE_LOCATION_LAT   "59.292840"
+#define DEVICE_LOCATION_LON   "18.100400"      
+#define DEVICE_SEND_INTERVAL  1800000             //default 3600000 ms = 1 hour
+
+#define WIFI_SSID             "***"
+#define WIFI_PASS             "***"
+
+
+#define MESSAGE_MAX_LEN       256
+#define EEPROM_SIZE           512
+#define SSID_LEN              32
+#define PASS_LEN              32
+#define CONNECTIONSTRING_LEN  256
